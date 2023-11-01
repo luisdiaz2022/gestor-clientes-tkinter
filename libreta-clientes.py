471dee63c563ef56e36e3f8d74aadf458ac3b882
Luis <luisedgucv@gmail.com>
2023-10-31 21:53:25 -0500

    Created function to add user and delete user, finished CRM project

--- libreta-clientes.py
+++ libreta-clientes.py
@@ -20,11 +20,76 @@ cursor.execute("""
                 );      
 """)
 
+def render_clientes():
+    rows = cursor.execute("SELECT * FROM cliente").fetchall()
+
+    tree.delete(*tree.get_children())
+
+    for row in rows:
+        tree.insert('', END, row[0], value=(row[1], row[2], row[3]))
+
+def insertar(cliente):
+    cursor.execute("""
+                    INSERT INTO cliente (nombre, telefono, empresa) VALUES (?, ?, ?)
+                   """, (cliente['nombre'], cliente['telefono'], cliente['empresa']))
+    conn.commit()
+    render_clientes()
+
 def nuevo_cliente():
-    pass
+    def guardar():
+        if not nombre.get():
+            messagebox.showerror('Error', 'El Nombre es obligatorio')
+            return
+        if not telefono.get():
+            messagebox.showerror('Error', 'El Nombre es obligatorio')
+            return
+        if not empresa.get():
+            messagebox.showerror('Error', 'El Nombre es obligatorio')
+            return
+        
+        cliente = {
+            'nombre': nombre.get(),
+            'telefono': telefono.get(),
+            'empresa': empresa.get()
+        }
+        insertar(cliente)
+        top.destroy()
+
+    top = Toplevel()
+    top.title('Nuevo Cliente')
+
+    lnombre = Label(top, text='Nombre')
+    nombre = Entry(top, width=40)
+    lnombre.grid(row=0, column=0)
+    nombre.grid(row=0, column=1)
+
+    ltelefono = Label(top, text='Telefono')
+    telefono = Entry(top, width=40)
+    ltelefono.grid(row=1, column=0)
+    telefono.grid(row=1, column=1)
+
+    lempresa = Label(top, text='Empresa')
+    empresa = Entry(top, width=40)
+    lempresa.grid(row=2, column=0)
+    empresa.grid(row=2, column=1)
+
+    btn_guardar = Button(top, text='Guardar', command=guardar)
+    btn_guardar.grid(row=3, column=1)
+
+    top.mainloop()
 
 def eliminar_cliente():
-    pass
+    id = tree.selection()[0]
+
+    cliente = cursor.execute("SELECT * FROM cliente WHERE id = ?", (id, )).fetchone()
+    respuesta = messagebox.askokcancel('Seguro?', 'Estas seguro de querer eliminar el cliente ' + cliente[1] + '?')
+    if respuesta:
+        cursor.execute("DELETE FROM cliente WHERE id = ?", (id, ))
+        conn.commit()
+        render_clientes()
+    else:
+        pass
+    
 
 button_cliente = Button(root, text='Nuevo Cliente', command=nuevo_cliente)
 button_cliente.grid(column=0, row=0)
@@ -43,5 +108,7 @@ tree.heading('Nombre', text='Nombre')
 tree.heading('Telefono', text='Telefono')
 tree.heading('Empresa', text='Empresa')
 tree.grid(column=0, row=1, columnspan=2)
+
+render_clientes()
 
 root.mainloop()
